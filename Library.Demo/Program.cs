ba1a7f5e50b5f02e577c362a4784e3f45a47ab96
Marina Paley <marina_paley@mail.ru>
2021-10-18 13:51:46 +0300

    Изменила свойства проекта на .NET 5.0

---
+++ Library.Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Domain;
+
+namespace Library.Demo
+{
+    using System;
+    using Library.Demo;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book(1, "Война и мир");
+            var author = new Author(1, "Толстой", "Лев", "Николаевич");
+
+            Console.WriteLine($"{book} {author}");
+        }
+    }
+}
---
+++ Library.Domain/Book.cs
@@ -0,0 +1,38 @@
+﻿namespace Domain
+{
+    using Staff.Extensions;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Книга.
+    /// </summary>
+    public class Book
+    {
+        public Book(int id, string title)
+        {
+            Id = id;
+
+            var trimmedTitle = title.TrimOrNull();
+
+            if (trimmedTitle == null)
+            {
+                throw new ArgumentNullException(nameof(title));
+            }
+
+            Title = trimmedTitle;
+        }
+
+        public int Id { get; protected set; }
+
+        public string Title { get; protected set; }
+
+        public ISet<Author> Authors { get; protected set; } = new HashSet<Author>();
+
+        public override string ToString()
+        {
+            // return $"{this.Title} {this.Authors.Join()}".Trim();
+            return $"{Title} {string.Join(", ", Authors)}"; //.Trim();
+        }
+    }
+}
